eab95562092074b6c2b9688b17d4e3f502a0ac41
Maélie Lenard <maelie.lenard@gmail.com>
2019-11-15 12:30:57 +0100

    Add semicolon, and start config for the doc

--- storybook/stories/DatePicker.stories.js
+++ storybook/stories/DatePicker.stories.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import DatePicker from '../../src/DatePicker';
-import { withKnobs, text, select } from "@storybook/addon-knobs";
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 
 export default {
   title: 'Date Picker',
-  decorators: [withKnobs]
-}
+  decorators: [withKnobs],
+};
 
 export const datePicker = () => {
   const styles = ['darkTheme', 'mediumTheme', 'lightTheme'];
 
-   return <DatePicker label={text('label', 'birthday')} style={select('style', styles)} />;
-}
+  return <DatePicker label={text('label', 'birthday')} style={select('style', styles)} />;
+};
