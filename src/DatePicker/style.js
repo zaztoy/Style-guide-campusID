ef7b1bc5f4bfcfab99b73e6cd5baeb7033b7e8f2
Maélie Lenard <maelie.lenard@gmail.com>
2020-01-09 15:58:03 +0100

    Use globals style

--- src/DatePicker/style.js
+++ src/DatePicker/style.js
@@ -1,56 +1,58 @@
+import globals from './../Theme/jssGlobals';
+
 export default {
-     darkTheme: {
-         '& input' : {
-             backgroundColor : '#404040',
-             color:'#DCDCDC',
-             borderBottom : '2px solid #181818',
-             padding : '10px',
-             borderRadius : '4px',
-             boxShadow : '2px 2px 2px 2px rgba(200,200,200,0.6)',
-             '&:hover' : {
-                backgroundColor : '#505050',
-             },
-         },
-         '& .MuiInput-underline' : {
-            '&:after' : {
-                borderBottom : '2px solid rgba(197, 0, 0, 6)',
-             },
-        }
+  darkTheme: {
+    '& input': {
+      backgroundColor: globals.backgroundColor.darkTheme,
+      color: globals.color.darkTheme,
+      borderBottom: '2px solid #181818',
+      padding: globals.spacing.md,
+      borderRadius: '4px',
+      boxShadow: '2px 2px 2px 2px rgba(200,200,200,0.6)',
+      '&:hover': {
+        backgroundColor: '#505050',
+      },
     },
-    mediumTheme: {
-        '& input' : {
-            backgroundColor : '#D3D3D3',
-            color:'#404040',
-            borderBottom : '2px solid rgba(197, 0, 0, 6)',
-            padding : '10px',
-            borderRadius : '4px',
-            boxShadow : '2px 2px 2px 2px rgba(200,200,200,0.6)',
-            '&:hover' : {
-               backgroundColor : '#BEBEBE',
-            },
-        },
-        '& .MuiInput-underline' : {
-            '&:after' : {
-                borderBottom : '2px solid rgba(197, 0, 0, 6)',
-             },
-        }
+    '& .MuiInput-underline': {
+      '&:after': {
+        borderBottom: '2px solid rgba(197, 0, 0, 6)',
+      },
     },
-    lightTheme: {
-        '& input' : {
-            backgroundColor : '#F5F5F5',
-            color: '#909090',
-            borderBottom : '2px solid #D3D3D3',
-            padding : '10px',
-            borderRadius : '4px',
-            boxShadow : '2px 2px 2px 2px rgba(200,200,200,0.6)',
-            '&:hover' : {
-               backgroundColor : '#E0E0E0',
-            },
-        },
-        '& .MuiInput-underline' : {
-            '&:after' : {
-            borderBottom : '2px solid rgba(197, 0, 0, 6) !important',
-             },
-        }
-    }
-}
+  },
+  mediumTheme: {
+    '& input': {
+      backgroundColor: globals.backgroundColor.mediumTheme,
+      color: globals.color.mediumTheme,
+      borderBottom: '2px solid rgba(197, 0, 0, 6)',
+      padding: globals.spacing.md,
+      borderRadius: '4px',
+      boxShadow: '2px 2px 2px 2px rgba(200,200,200,0.6)',
+      '&:hover': {
+        backgroundColor: '#BEBEBE',
+      },
+    },
+    '& .MuiInput-underline': {
+      '&:after': {
+        borderBottom: '2px solid rgba(197, 0, 0, 6)',
+      },
+    },
+  },
+  lightTheme: {
+    '& input': {
+      backgroundColor: globals.backgroundColor.lightTheme,
+      color: globals.color.lightTheme,
+      borderBottom: '2px solid #D3D3D3',
+      padding: globals.spacing.md,
+      borderRadius: '4px',
+      boxShadow: '2px 2px 2px 2px rgba(200,200,200,0.6)',
+      '&:hover': {
+        backgroundColor: '#E0E0E0',
+      },
+    },
+    '& .MuiInput-underline': {
+      '&:after': {
+        borderBottom: '2px solid rgba(197, 0, 0, 6) !important',
+      },
+    },
+  },
+};
