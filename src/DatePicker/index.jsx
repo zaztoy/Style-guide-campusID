cbe6f29097a79bb6df1f84a4240cf8c83c6d26c9
Maélie Lenard <maelie.lenard@gmail.com>
2020-01-09 15:57:31 +0100

    use moment js for default value

--- src/DatePicker/index.jsx
+++ src/DatePicker/index.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import { createUseStyles } from "react-jss";
+import { createUseStyles } from 'react-jss';
+import moment from 'moment';
 
-import styles from "./style";
+import styles from './style';
 
-const DatePicker = (props) => {
+const DatePicker = props => {
+  const today = moment().format('YYYY-MM-DD');
   const useStyles = createUseStyles(styles);
   const classes = useStyles();
 
-  const {label, style ='darkTheme'} = props;
+  const { label, style = 'darkTheme' } = props;
 
   return (
-      <TextField
-        classes={{root:classes[style]}}
-        id="date-picker-dialog"
-        label={label}
-        type="date"
-        defaultValue="1994-07-08"
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
+    <TextField
+      classes={{ root: classes[style] }}
+      id="date-picker-dialog"
+      label={label}
+      type="date"
+      defaultValue={today}
+      InputLabelProps={{
+        shrink: true,
+      }}
+    />
   );
-}
+};
 
-export default DatePicker
+export default DatePicker;
