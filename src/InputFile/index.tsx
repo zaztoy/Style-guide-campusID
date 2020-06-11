51e481a4e791e2ae7ab266cad814525d06eabb44
Jan.Vermeulen <jan.vermeulen@ricardo.ch>
2020-06-11 18:26:23 +0200

    avatar + fileUpload

---
+++ src/InputFile/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import styles from './style';
+
+type Props = {
+  children: React.ReactNode;
+  setFileData: (fileDataURL: any) => void;
+  image?: boolean;
+};
+
+const InputFile = ({ children, setFileData, image }: Props) => {
+  const classes = styles();
+
+  const readFile = (file: Blob): Promise<string> => {
+    return new Promise(resolve => {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => resolve(reader.result.toString()), false);
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const onImageChange = async (e: React.ChangeEvent<HTMLInputElement>): Promise<any> => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file: File = e.target.files[0];
+
+      const fileDataURL = await readFile(file);
+      setFileData(fileDataURL);
+    }
+  };
+
+  const onFileChange = async (e: React.ChangeEvent<HTMLInputElement>): Promise<any> => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file: File = e.target.files[0];
+      setFileData(file);
+    }
+  };
+
+  return (
+    <label htmlFor="avatar-file" className={classes.label}>
+      {children}
+      <input
+        id="avatar-file"
+        accept={image ? 'image/png, image/jpeg, image/jpg' : null}
+        data-testid="avatar-input-file"
+        name="avatar"
+        type="file"
+        className={classes.hiddenInputFile}
+        onChange={image ? onImageChange : onFileChange}
+        onClick={e => {
+          // Reset the value for every click, so we always have a change
+          e.currentTarget.value = '';
+        }}
+      />
+    </label>
+  );
+};
+
+export default InputFile;
---
+++ storybook/stories/SimpleFileUploader.stories.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import Avatar from '../../src/Avatar';
+import InputFile from '../../src/InputFile';
+
+import { withKnobs, text, select, boolean } from '@storybook/addon-knobs';
+
+export default {
+  title: 'Simple File-Uploader',
+  decorators: [withKnobs],
+};
+
+export const FileUploaderStory = () => {
+  const [file, setFile] = useState(null);
+
+  const imageUpload = boolean('Image', false);
+  const setFileData = (fileData: any) => {
+    setFile(fileData);
+    console.log(fileData);
+  };
+
+  return imageUpload ? (
+    <>
+      <InputFile setFileData={setFileData} image>
+        <span
+          style={{
+            backgroundColor: 'red',
+            fontSize: '1.25em',
+            fontWeight: 700,
+            color: 'white',
+            padding: 20,
+            borderRadius: 4,
+          }}
+        >
+          Upload an image
+        </span>
+      </InputFile>
+      <img src={file} width={200} />
+    </>
+  ) : (
+    <>
+      <InputFile setFileData={setFileData}>
+        <span
+          style={{
+            backgroundColor: 'red',
+            fontSize: '1.25em',
+            fontWeight: 700,
+            color: 'white',
+            display: 'inline-block',
+            padding: 20,
+            borderRadius: 4,
+          }}
+        >
+          Upload a file
+        </span>
+      </InputFile>
+      {file && file.size && (
+        <>
+          <div>filename : {file && file.name}</div>
+          <div>fileSize: {file && `${file.size} octet`}</div>
+          <div>fileType: {file && file.type}</div>
+        </>
+      )}
+    </>
+  );
+};
