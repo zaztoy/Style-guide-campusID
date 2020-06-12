bea03a50006b6e18399d4f7dd0436a4bed68ffbe
Maélie Lenard <maelie.lenard@gmail.com>
2020-06-12 11:28:45 +0200

    wip

--- storybook/stories/Card.stories.jsx
+++ storybook/stories/Card.stories.jsx
@@ -3,10 +3,60 @@ import ImageCard from '../../src/Card/ImageCard';
 import SimpleCard from '../../src/Card/SimpleCard';
 import { withKnobs, text, select } from '@storybook/addon-knobs';
 
+import {
+  Title,
+  Subtitle,
+  Description,
+  Primary,
+  Props,
+  Stories,
+} from '@storybook/addon-docs/blocks';
+
 export default {
   title: 'Card',
   decorators: [withKnobs],
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Props />
+          <Stories />
+        </>
+      ),
+    },
+  },
 };
+
+export const defDocsPage = () => (
+  <>
+    <div>Une CARD est un composant qui sert à afficher des informations.</div>
+    <hr />
+    <br />
+    <h2> Composant 'Image Card' : </h2>
+    <h4>PROPS</h4>
+    <ul>
+      <li>
+        Title : <i>string</i>
+      </li>
+      <li>
+        Style : <i>Enum ['darkTheme', 'mediumTheme', 'lightTheme']</i>
+      </li>
+      <li>
+        Title : <i>string</i>
+      </li>
+      <li>
+        Text : <i>string</i>
+      </li>
+      <li>
+        Image : <i>string</i>
+      </li>
+    </ul>
+  </>
+);
 
 export const imageCard = () => {
   const styles = ['darkTheme', 'mediumTheme', 'lightTheme'];
@@ -18,7 +68,7 @@ export const imageCard = () => {
         'http://www.campusid.fr/wp-content/uploads/2018/02/logo-carre-2.png'
       )}
       title={text('Title', 'CampusID')}
-      style={select('style', styles)}
+      style={select('Style', styles)}
       text={text('Text', 'You can put some text if you need')}
     />
   );
